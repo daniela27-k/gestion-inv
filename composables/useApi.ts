18007671c88640b87bc7ce1c18eeffba55287e77
daniela27-k <hoyoslozanodaniela@gmail.com>
2026-05-13 18:42:17 -0500

    fix: usar apiBaseUrl del runtimeConfig en useApi

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,17 +1,14 @@
 //composables/useApi.ts
-
 import type { UseFetchOptions } from 'nuxt/app';
 
 export function useApi<T>(url: string | (() => string), opts: UseFetchOptions<T> = {}) {
+  const config = useRuntimeConfig();
+
   const defaults: UseFetchOptions<T> = {
-    baseURL: 'http://localhost:3001', // URL del backend en NestJS
-    credentials: 'include', // La clave para enviar la cookie
+    baseURL: config.public.apiBaseUrl || 'http://localhost:3001',
+    credentials: 'include',
   };
-  
 
-  // Combina las opciones predeterminadas con las opciones pasadas por el usuario
   const options = { ...defaults, ...opts };
-
-  // Ahora, useFetch generará la clave única automáticamente
   return useFetch(url, options as any);
 }
